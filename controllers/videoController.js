1628b7bfdd7132e845f78ba3aead9c05489d8a73
bongbong <60419514+bongbongche@users.noreply.github.com>
2020-04-19 16:32:24 +0900

    #2.18~#2.25

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,33 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
-export const upload = (req, res) =>
+import { videos } from "../db";
+import routes from "../routes";
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
+};
+
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { title, file, description },
+  } = req;
+  //To Do: Upload and Save videos
+  res.redirect(routes.videoDetail(111111));
+};
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 111111,
+    title: "Video 1",
+    description: "This is Video 1",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 999999,
+      name: "Hyun",
+      email: "hyun@seo.com",
+    },
+  },
+  {
+    id: 222222,
+    title: "Video 2",
+    description: "This is Video 1",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 999999,
+      name: "Hyun",
+      email: "hyun@seo.com",
+    },
+  },
+  {
+    id: 333333,
+    title: "Video 3",
+    description: "This is Video 1",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 999999,
+      name: "Hyun",
+      email: "hyun@seo.com",
+    },
+  },
+  {
+    id: 444444,
+    title: "Video 4",
+    description: "This is Video 1",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 999999,
+      name: "Hyun",
+      email: "hyun@seo.com",
+    },
+  },
+];
